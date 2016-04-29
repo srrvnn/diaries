b440cd5c3f31763b618c6a2d4b9ad0521c90ba10
Saravanan Ganesh <saravanan.ganesh@ensighten.com>
2016-04-29 00:40:16 -0700

    change email to srrvnn@gmail.com

--- js/all.js
+++ js/all.js
@@ -378,7 +378,7 @@ var DiariesFooter = React.createClass({
     render: function() {
         return (
             <ul className="diaries-footer">
-                <li> Built to land a phone screen at Facebook. <a href="mailto:saravanan@alumni.usc.edu?subject=Hello from Facebook" target="_blank">Recruiter?</a> </li>
+                <li> Built to land a phone screen at Facebook. <a href="mailto:srrvnn@gmail.com?subject=Hello from Facebook" target="_blank">Recruiter?</a> </li>
                 <li> <a href="http://srrvnn.me/resume" target="_blank">Resume</a> </li>
                 <li> <a href="http://github.com/srrvnn/diaries" target="_blank">Source Code</a> </li>
                 <li> <a href="http://srrvnn.me/diaries/privacypolicy.htm" target="_blank">Privacy Policy</a> </li>
